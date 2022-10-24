33274c155dc03e86d47137e40d4e7184acf411f7
Batuhan Kaya <73339365+batuky@users.noreply.github.com>
2022-10-24 09:54:25 +0300

    Created a repository and added first week codes.
    this week about gray scaling

---
+++ week1/method3.py
@@ -0,0 +1,21 @@
+import cv2
+ 
+# Load the input image
+image = cv2.imread('C:\\yazilim\\imageProcessing\\images\\grayScalingImage.jpg')
+image = cv2.resize(image,(1280,720)) 
+# Obtain the dimensions of the image array
+# using the shape method
+(row, col) = image.shape[0:2]
+ 
+# Take the average of pixel values of the BGR Channels
+# to convert the colored image to grayscale image
+for i in range(row):
+    for j in range(col):
+        # Find the average of the BGR pixel values
+        image[i, j] = sum(image[i, j]) * 0.33
+ 
+cv2.imshow('Grayscale Image', image)
+cv2.waitKey(0)
+ 
+# Window shown waits for any key pressing event
+cv2.destroyAllWindows()
